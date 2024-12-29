9fcd859dd56ce0380f16aa474812d9e274a22b2d
nnayk <79224511+nnayk@users.noreply.github.com>
2024-12-29 10:22:32 -0800

    Starter code

---
+++ temperature.py
@@ -0,0 +1,18 @@
+'''
+You will write a function called "celsiusToFahrenheit" to convert temperature in Celsius to Fahrenheit. These two values are related
+by the formula:
+celsius * 1.8 = fahrenheit – 32
+As an input to the program, you will have to prompt the user to enter a value in Celsius. For prompting
+and accepting the user for an input, use input(msg) in your code. It presents a prompt to the user (you
+will have to replace ‘msg’ with a prompt message in quotes), gets input from the user and stores the
+input entered by the user in a string. You will have to write code for displaying the equivalent Fahrenheit
+value on screen.
+Test your function with known inputs to verify if your output is correct or not.
+'''
+
+def celsiusToFahrenheit(temperature):
+    pass
+    # write your code here (and delete the pass statement above)
+
+
+# write a couple tests for this program. They should call the function celsiusToFahrenheit and check that the returned value is correct
